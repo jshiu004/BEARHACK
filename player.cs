aff2cb552c16776fa4fa6d4076a73c6662dfdc6b
Bestin Watts <bwatt004@xe-01.cs.ucr.edu>
2024-04-27 23:00:29 -0700

    changed player

--- player.cs
+++ player.cs
@@ -7,6 +7,10 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     private Food[] foodData;
     private main mainClass;
+
+
+    private Food currFood;
+    private Food nextFood;
     void Start()
     {
         mainClass = new main();
@@ -18,7 +22,42 @@ public class player : MonoBehaviour
 
     private void startGame()
     {
+        currFood = mainClass.randomFood(ref foodData);
+
+        nextFood = mainClass.randomFood(ref foodData);
+
+        while (currFood == nextFood)
+        {
+            nextFood = mainClass.randomFood(ref foodData);
+        }
+
 
     }
+
+    public void highClick()
+    {
+        if (nextFood.getCalories() >= currFood.getCalories())
+        {
+            correct();
+        } else
+        {
+            lose();
+        }
+    }
+
+    public void lowClick()
+    {
+        if (nextFood.getCalories() <= currFood.getCalories())
+        {
+            correct();
+        }
+        else
+        {
+            lose();
+        }
+    }
+
+
+
 
 }
