f957a9cad5ea4f40bf48c6330351d57d2d250646
Bestin Watts <bwatt004@xe-01.cs.ucr.edu>
2024-04-27 18:48:49 -0700

    added image animatio

--- imageBehaviour.cs
+++ imageBehaviour.cs
@@ -11,8 +11,13 @@ public class imageBehaviour : MonoBehaviour
 
     private Vector3 topStart = new Vector3(-4.8f, 2.58f, 0f);
     private Vector3 bottomStart = new Vector3(4.8f, -2.55f, 0f);
+
+    private float animationTime = 1.5f;
+    private float elapsedTime = 0f;
+    private float percent = 0f;
     void Start()
     {
+        this.enabled = false;
         if (this.tag == "top")
         {
             isTop = true;
@@ -20,7 +25,7 @@ public class imageBehaviour : MonoBehaviour
         {
             isTop = false;
         }
-
+        Debug.Log("working");
         setUp();
     }
 
@@ -34,6 +39,11 @@ public class imageBehaviour : MonoBehaviour
         {
             transform.position = bottomStart;
         }
+        Debug.Log("working 2");
+
+
+        //testing
+        slideIn();
     }
 
 
@@ -44,10 +54,30 @@ public class imageBehaviour : MonoBehaviour
 
     public void slideIn()
     {
-
+        this.enabled = true;
     }
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isTop)
+        {
+           // Debug.Log("workingTop");
+
+            elapsedTime += Time.deltaTime;
+
+            percent = elapsedTime / animationTime;
+
+            transform.position = Vector3.Lerp(topStart, topEnd, Mathf.SmoothStep(0f, 1f, percent));
+
+        }
+        else
+        {
+          //  Debug.Log("workingBottom");
+
+            elapsedTime += Time.deltaTime;
+
+            percent = elapsedTime / animationTime;
+
+            transform.position = Vector3.Lerp(bottomStart, bottomEnd, Mathf.SmoothStep(0f, 1f, percent));
+        }
     }
 }
